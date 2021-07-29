7f94a47537192536762b4d59d3c1fd65250a6ceb
Thales Gonçalves <thalesgoncalves3@gmail.com>
2021-07-29 18:36:03 -0300

    Integrando js e criando componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+import React, { PureComponent } from 'react';
 
-
-function App() {
-  return (
-   <h1>Meu app react</h1>
-  );
+class App extends Component {
+  render(){
+    return (
+      <section>
+        <FormularioCadastro/>
+        <ListaDeNotas/>
+      </section>
+    );
+  }
 }
 
 export default App;
