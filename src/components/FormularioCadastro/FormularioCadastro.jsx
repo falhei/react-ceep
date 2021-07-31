2eeb39c0ef93785016032ef8e992206742f17385
Thales Gonçalves <thalesgoncalves3@gmail.com>
2021-07-31 15:28:18 -0300

    Adicionando bind

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+
+  constructor(){
+    super();
+    this.titulo = "";
+  }
+
+    handleMudancaTitulo(evento){
+      this.titulo = evento.target.value;
+      console.log(this.titulo);
+    }
+
+
   render() {
     return (
       <form className="form-cadastro">
-        <input type="text" placeholder="Título" className="form-cadastro_input"></input>
-        <textarea placeholder="Escreva sua nota..." className="form-cadastro_input" rows={15}></textarea>
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
+        ></input>
+        <textarea
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          rows={15}
+        ></textarea>
         <button className="form-cadastro_submit">Criar Nota</button>
       </form>
     );
